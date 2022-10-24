ec3ac58ae4e225ef6cfe0ce93a60c90995078025
jaehyeoklim <jaehyeoklim99@gmail.com>
2022-10-24 16:07:12 +0900

    initialView 추가

---
+++ HARU/HARU/initialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  initialViewController.swift
+//  HARU
+//
+//  Created by Jaehyeok Lim on 2022/10/24.
+//
+
+import UIKit
+
+class initialViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func viewLayout() {
+        view.backgroundColor = UIColor.white
+    }
+}
